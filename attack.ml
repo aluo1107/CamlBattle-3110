0644e9db0ab3c5bd93fbf82d79832b3a1fcc01ba
Ada Luo <al2342@cornell.edu>
2021-03-28 20:45:01 -0400

    finished matching attack and heal

--- attack.ml
+++ attack.ml
@@ -1,8 +1,8 @@
 open Camltypes
 
-let attack_base = 10.0
+let attack_base = 10
 
-let heal_base = 10.0
+let heal_base = 10
 
 exception UnknownMove of move_t
 
@@ -16,20 +16,22 @@ let get_multiplier effect = effect_multi effect
     returns how much damage is done to the victim. Requires: [attacker]
     and [victim] be a valid Camltypes.t, [stage] be a valid Stages.t and
     [move] be a valid type t *)
-
 let attack_move attacker victim stage move =
   let attack_multiplier =
     get_multiplier
       (attack_multi (caml_type attacker) (caml_type victim))
   in
-  let attack_damage_type = attack_multiplier *. attack_base in
+  let attack_damage_type =
+    int_of_float attack_multiplier * attack_base
+  in
   let attack_multi_stage =
     get_multiplier (attack_stage_multi attacker stage)
   in
-  attack_damage_type *. attack_multi_stage
+  attack_damage_type * int_of_float attack_multi_stage
 
-(* let defense_move attacker victim (stage : Stages.t) move =
-
-   let move_done attacker victim stage move = match move with | Attack
-   -> attack_move attacker victim stage move | Defense -> | Heal -> | x
-   -> raise (UnknownMove x) *)
+let defense_move attacker victim (stage : Stages.t) move =
+  match move with
+  | Attack -> updated_hp victim (attack_move attacker victim stage move)
+  | Defense -> 10
+  | Heal -> updated_hp victim (-(heal_base * attacker.level))
+  | x -> raise (UnknownMove x)
